c5db20d78422d4ece016ec3bb08d1b36a7ac29fe
Karol P <kpawlx@gmail.com>
2017-07-13 03:51:02 +0200

    Offer - description tables from CSV files; correction of building plans

--- js/agency.js
+++ js/agency.js
@@ -1,3 +1,29 @@
+function set_description_table(table_template, typ_budynku, strona, poziom) {
+  Papa.parse("T_" + typ_budynku + "_" + strona + "_" + poziom + ".csv", {
+    download: true,
+    header: true,
+    skipEmptyLines: true,
+    complete: function(results) {
+      // console.log(results);
+      var table_html = table_template(results);
+      $("." + "T_" + typ_budynku + "_" + strona + "_" + poziom).html(table_html);
+    }
+  });
+}
+
+function set_description_tables(table_template) {
+  var typy_budynkow = ['A1', 'A2', 'A3', 'B1', 'C1']
+  var strony = ['L', 'P']
+  var poziomy = ['1', '2']
+  $(typy_budynkow).each(function(typ_budynku_idx, typ_budynku) {
+    $(strony).each(function(strona_idx, strona) {
+      $(poziomy).each(function(poziom_idx, poziom) {
+        set_description_table(table_template, typ_budynku, strona, poziom);
+      });
+    });
+  });
+}
+
 (function($) {
     "use strict"; // Start of use strict
 
@@ -33,47 +59,39 @@
       $(this).text($(this).text().replace(' małpka ','@'))
     });
 
-    //offer houses
-    var source   = $("#offer-houses-template").html();
-    var template = Handlebars.compile(source);
-    Papa.parse("/offer_houses.csv", {
+
+    //offer houses templates
+    var offer_houses_template_source   = $("#offer-houses-template").html();
+    var offer_houses_template = Handlebars.compile(offer_houses_template_source);
+    var offer_houses_modals_template_source   = $("#offer-houses-modals-template").html();
+    var offer_houses_modals_template = Handlebars.compile(offer_houses_modals_template_source);
+
+    //offer houses - description tables template
+    var offer_houses_table_template_source   = $("#offer-houses-table-template").html();
+    var offer_houses_table_template = Handlebars.compile(offer_houses_table_template_source);
+
+    var status_type_map = {
+      'Dostępny': 'success',
+      'Rezerwacja': 'warning',
+      'Sprzedany': 'danger'
+    };
+
+    Papa.parse("offer_houses.csv", {
     	download: true,
       header: true,
       skipEmptyLines: true,
     	complete: function(results) {
-    		console.log(results);
-        var html = template(results);
-        $("#offer_houses").html(html);
+        $(results.data).each(function(idx, element) {
+          element.typ_statusu = status_type_map[element.status];
+        });
+        var offer_houses_html = offer_houses_template(results);
+        $("#offer_houses").html(offer_houses_html);
+        var offer_houses_modals_html = offer_houses_modals_template(results);
+        $("#offer_modals").html(offer_houses_modals_html);
+
+        set_description_tables(offer_houses_table_template);
     	}
     });
-    // var data = {
-    //   houses: [
-    //     {
-    //       numer: "1-L",
-    //       powierzchnia: 137,
-    //       cena: "559,900",
-    //       status: "SPRZEDANY"
-    //     },
-    //     {
-    //       numer: "1-P",
-    //       powierzchnia: 137,
-    //       cena: "559,999",
-    //       status: "DOSTĘPNY"
-    //     },
-    //     {
-    //       numer: "2-L",
-    //       powierzchnia: 137,
-    //       cena: "559,900",
-    //       status: "SPRZEDANY"
-    //     },
-    //     {
-    //       numer: "2-P",
-    //       powierzchnia: 137,
-    //       cena: "559,999",
-    //       status: "DOSTĘPNY"
-    //     }
-    //   ]
-    // };
 
 
 })(jQuery); // End of use strict
